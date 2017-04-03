3704c3d37195da6211160717bcfe600f628e0a5a
prefrontalvortex <mikemcdermott23@gmail.com>
2017-04-03 19:12:05 -0400

    some scaffolding work on menu. got recursive calls working and stuff

--- menu.py
+++ menu.py
@@ -1,40 +1,103 @@
-from __future__ import print_function, division
-from builtins import input
+class Choice(object):
+    def __init__(self, key_or_tup, name=None, callback=None):
+        if isinstance(key_or_tup, Choice):
+            key = key_or_tup.key_or_tup
+            name = key_or_tup.name
+            callback = key_or_tup.callback
+        elif isinstance(key_or_tup, (tuple, list)):
+            if len(key_or_tup) == 3:
+                key, name, callback = key_or_tup
+            elif len(key_or_tup) == 2:
+                key, name = key_or_tup
+            elif len(key_or_tup) == 1:
+                key = key_or_tup[0]
+            else:
+                raise ValueError('Invalid menu choice specification list')
+        elif isinstance(key_or_tup, str):
+            key = key_or_tup
+        else:
+            raise ValueError('Invalid menu choice specification list')
+        self.key = key
+        self.name = name
+        self.callback = callback
+
+    def __str__(self):
+        return '{: >2}: {}'.format(self.key, self.name)
+
+    def __call__(self, *args, **kwargs):
+        if self.callback is not None:
+            return self.callback(*args, **kwargs)
 
 
-class MenuPicker(object):
-    def __init__(self, choicedict):
-        self.choicedict = choicedict
+def return_foo():
+    return 'foo'
+
+
+class Quit(Choice):
+    def __init__(self):
+        super().__init__('q', 'Quit')
+
+    def __call__(self):
+        print('Quitting')
+
+
+class Menu(Choice):
+    '''
+    A special choice object, which when called, returns a new context menu
+    '''
+
+    def __init__(self, key, name=None, choices=None, loop_on_invalid=False):
+        print(name + 'init')
+        self.loop_on_invalid = loop_on_invalid
+        self.quit = Quit()
+        choices = [self.quit] if choices is None else [self.quit] + choices
+        self.choices = choices
+        self.name = name
+        super().__init__(key_or_tup=key, name=name, callback=self)
+        print(name + 'super called')
+
+    def get_item(self, key):
+        lookup = {choice.key: choice for choice in self.choices}
+        if key == '' and self.loop_on_invalid:
+            return Choice('', 'Nothing selected', lambda: print('Nothing selected'))
+        elif key in lookup:
+            return lookup[key]
+        else:
+            print('Invalid entry!')
+            if self.loop_on_invalid:
+                print('Enter menu selection: ')
+            else:
+                self.quit()
 
     def show_menu(self):
-        print ('0: Quit')
-        i = 1
-        keystruct = {} # this is to prevent the order from getting screwed up. SHOULD be the same, but I am leery
-        for key, value in self.choicedict.items():
-            if isinstance(value, dict):
-                marker = '+'
-                keystruct.update({i: value})
+        print('calling show_menu' + self.name)
+        print(self.name)
+        for choice in self.choices:
+            print(choice)
 
-            else:
-                marker = '.'
-                keystruct.update({i: key})
-
-            print('{}:[{}] {}'.format(i, marker, key))
-            i += 1
-        return keystruct
+    def add(self, choice):
+        self.choices.append(Choice(choice))
 
     def user_pick_menu(self):
-        keystruct = self.show_menu()
+        #         keystruct = self.show_menu()
         reply = input("Enter menu selection: ")
-        if reply == '' or reply == '0' or reply is None:
-            print('Quitting')
-            return None
-        reply = int(reply)
-        print(keystruct[reply])
-        print('------')
-        if isinstance(keystruct[reply], dict):
-            submenu= MenuPicker(keystruct[reply])
-            return submenu.user_pick_menu()
-        else:
-            return keystruct[int(reply)]
+        return reply
 
+    #         if reply == '' or reply == '0' or reply is None:
+    #             print('Quitting')
+    #             return None
+    #         reply = int(reply)
+    #         print(keystruct[reply])
+    #         print('------')
+    #         if isinstance(keystruct[reply], dict):
+    #             submenu= MenuPicker(keystruct[reply])
+    #             return submenu.user_pick_menu()
+    #         else:
+    #             return keystruct[int(reply)]
+
+    def __call__(self, *args, **kwargs):
+        self.show_menu()
+        r = self.user_pick_menu()
+        choice = self.get_item(r)
+        if choice is not None:
+            return choice()
